95010817e7827b1ec41236dcf4ce4a4c6ca97367
Filip Smets <alemaaltevinden@gmail.com>
2016-03-09 17:31:28 +0100

    Add space and comments for components

--- client/main.js
+++ client/main.js
@@ -1,3 +1,51 @@
+/*
+	######################################
+	# Authentication Components          #
+	######################################
+*/
+
+/*
+	Register component
+*/
+
+
+
+/*
+	Login component
+*/
+
+
+
+/*
+	######################################
+	# Graphing Components                #
+	######################################
+*/
+
+
+
+/*
+	######################################
+	# Social Wall Components             #
+	######################################
+*/
+
+/*
+	Post component
+	Used to show wall posts
+*/
+
+/*
+	Wall component
+	Shows posts in walls
+*/
+
+/*
+	Post Form Component
+	This can be used to make posts on walls
+*/
+
+
 ReactDOM.render(
 	<h1>Hello, world!</h1>,
 	document.getElementById('example')
