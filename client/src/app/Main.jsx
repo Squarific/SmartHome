b52bcd68f4fd068cb5216872c6fe1829dfe37bd9
Filip Smets <alemaaltevinden@gmail.com>
2016-03-15 10:23:11 +0100

    Finish graphcard

--- client/src/app/Main.jsx
+++ client/src/app/Main.jsx
@@ -14,7 +14,7 @@ import MuiThemeProvider from 'material-ui/lib/MuiThemeProvider';
 import {RegisterForm, LoginForm} from './Authentication'
 import AppBar from 'material-ui/lib/app-bar';
 import HouseHoldCard from './HouseHoldCard';
-//import GraphCard from './Graphing';
+import {GraphCard} from './Graphing';
 
 const styles = {
   container: {
@@ -61,6 +61,28 @@ class Main extends React.Component {
 		onTouchTap={this.handleRequestClose}/>
 	);
 
+  const data = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+          {
+              label: "My First dataset",
+              fillColor: "rgba(220,220,220,0.5)",
+              strokeColor: "rgba(220,220,220,0.8)",
+              highlightFill: "rgba(220,220,220,0.75)",
+              highlightStroke: "rgba(220,220,220,1)",
+              data: [65, 59, 80, 81, 56, 55, 40],
+          },
+          {
+              label: "My Second dataset",
+              fillColor: "rgba(76,175,80,0.5)",
+              strokeColor: "rgba(151,187,205,0.8)",
+              highlightFill: "rgba(151,187,205,0.75)",
+              highlightStroke: "rgba(151,187,205,1)",
+              data: [28, 48, 40, 19, 86, 27, 90],
+          },
+      ],
+  };
+
 	return (
 	  <MuiThemeProvider muiTheme={muiTheme}>
 		<div style={styles.container}>
@@ -75,10 +97,22 @@ class Main extends React.Component {
 				prev="vorige week"
 				next="volgende week"/>
 			</div>
+      <div>
+        <GraphCard title="Wasmachine"
+                   subtitle="Verbruik"
+                   data={data}
+                   graphType="Bar"
+                   graphTypes={["Line", "Bar", "Radar"]}>
+          <div>Test lol</div>
+          <div>Omg xD</div>
+        </GraphCard>
+      </div>
 		</div>
 	  </MuiThemeProvider>
 	);
   }
 }
-
+/*
+  data={data}
+*/
 export default Main;
