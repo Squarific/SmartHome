c7b4fffa556794c4416605f01e9f5ea5a7799317
Filip Smets <alemaaltevinden@gmail.com>
2016-03-17 11:31:52 +0100

    Add some structure to householdlist

---
+++ client/src/app/PageComponents/HouseHoldList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import HouseHoldCard from '../Components/HouseHoldCard.jsx';
+
+/*
+	HouseHoldList Component
+
+	Props: {
+		userid: INT
+	}
+*/
+
+const HouseHoldList = React.createClass({
+	render: function() {
+		return ();
+	},
+});
+
+module.exports = {
+	HouseHoldList: HouseHoldList,
+};
