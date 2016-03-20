f4824f5548cc3257f9c41afb65578c561d84badd
David Danssaert <david.danssaert@gmail.com>
2016-03-20 22:02:11 +0100

    Hopefullyly fixed CORS..

--- server/SmartHome/settings.py
+++ server/SmartHome/settings.py
@@ -55,13 +55,13 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'SmartHome.urls'
@@ -138,12 +138,14 @@ REST_FRAMEWORK = {
     'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
 }
 
-CORS_ORIGIN_WHITELIST = [
-    '',
-    '*',
-    'localhost',
-    'localhost:8000',
-]
+CORS_ORIGIN_ALLOW_ALL = True
+
+#CORS_ORIGIN_WHITELIST = [
+#    '',
+#    '*',
+#    'localhost',
+#    'localhost:8000',
+#]
 
 SITE_ID = 1
 
