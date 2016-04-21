d9766be3cd8ff98555d5755c693fbde41f0179cd
Filip Smets <alemaaltevinden@gmail.com>
2016-04-21 15:02:51 +0200

    Add i18n

---
+++ client/src/app/Components/Translations.jsx
@@ -0,0 +1,16 @@
+const nl = {
+	noInternet: "Verbinding met de server is niet mogelijk. Heb je internet?",
+	requestError: "Er was een server probleem. De server antwoordde met status:",
+	JSONError: "JSON Parseer fout:",
+}
+
+const en = {
+	noInternet: "Could not connect to the server, either it is offline or you don't have internet.",
+	requestError: "Problem while trying to reach the server. Server responded with status:",
+	JSONError: "JSON Parse error:",
+}
+
+module.exports = {
+	nl,
+	en
+};
