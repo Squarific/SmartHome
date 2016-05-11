23c68683a12983b1e29af69db0aa1828ed41b2b3
Inventrax <inventrax@gmail.com>
2016-05-11 14:04:02 +0200

    Table works, no data from server yet

--- client/src/app/PageComponents/Friends.jsx
+++ client/src/app/PageComponents/Friends.jsx
@@ -2,20 +2,41 @@ import React from 'react';
 import Card from 'material-ui/lib/card/card';
 import CardActions from 'material-ui/lib/card/card-actions';
 import CardHeader from 'material-ui/lib/card/card-header';
+import Table from 'material-ui/lib/table/table';
+import TableHeaderColumn from 'material-ui/lib/table/table-header-column';
+import TableRow from 'material-ui/lib/table/table-row';
+import TableHeader from 'material-ui/lib/table/table-header';
+import TableRowColumn from 'material-ui/lib/table/table-row-column';
+import TableBody from 'material-ui/lib/table/table-body';
+import TableFooter from 'material-ui/lib/table/table-footer';
+import TextField from 'material-ui/lib/text-field';
+import Toggle from 'material-ui/lib/toggle';
 import {green500, grey500} from 'material-ui/lib/styles/colors';
-import {FriendTable} from '../Components/FriendTable';
 
 const styles = {
 	friends: {
 		color: green500,
 		padding: 32,
 	},
+	propContainer: {
+		width: 200,
+		overflow: 'hidden',
+		margin: '20px auto 0',
+	},
+	propToggleHeader: {
+		margin: '20px auto 10px',
+	},
 }
+
+
 
 const Friends = React.createClass({
 	getInitialState: function () {
-		const value = '';
-		return {value};
+		return {
+			loading: true,
+			friends: [],
+			myName: "Loading...",
+		};
 	},
 	handleChange: function (event, index, value) {
 		this.setState({value});
@@ -47,14 +68,82 @@ const Friends = React.createClass({
 		}.bind(this));
 	},
 	render: function() {
+		
+		let FriendList= [];
+		let friend = {};
+
+
+		if (!this.state.loading) {
+			for (let i = 0; i < 5; i++) {
+				/*if (!this.state.friends[i].attributes.read)*/
+					FriendList.push(friend = {ranking: i+1, name: 'John Smith', usage: 'Employed'});
+			}
+		}
+
+		const tableData = FriendList;/*[
+		  {
+		  	ranking: '1',
+		    name: 'John Smith',
+		    usage: 'Employed',
+		  },
+		  {
+		  	ranking: '2',
+		    name: 'Randal White',
+		    usage: 'Unemployed',
+		  },
+		  {
+		  	ranking: '3',
+		    name: 'Stephanie Sanders',
+		    usage: 'Employed',
+		  },
+		  {
+		  	ranking: '4',
+		    name: 'Steve Brown',
+		    usage: 'Employed',
+		  },
+		  {
+		  	ranking: '5',
+		    name: 'Joyce Whitten',
+		    usage: 'Employed',
+		  },
+		  {
+		  	ranking: '6',
+		    name: 'Samuel Roberts',
+		    usage: 'Employed',
+		  },
+		  {
+		  	ranking: '7',
+		    name: 'Adam Moore',
+		    usage: 'Employed',
+		  },
+		];*/
+		
 		return (
 			<Card>
 			<CardHeader
 				title="Friends"
 				subtitle={this.state.myName}/>
-
-			{FriendTable}
-
+				<div>
+			      <Table
+			        height={this.state.height}>
+			        <TableHeader>
+			          <TableRow>
+			            <TableHeaderColumn tooltip="The Rank">Ranking</TableHeaderColumn>
+			            <TableHeaderColumn tooltip="The Name">Name</TableHeaderColumn>
+			            <TableHeaderColumn tooltip="The Usage">Usage</TableHeaderColumn>
+			          </TableRow>
+			        </TableHeader>
+			        <TableBody>
+			          {tableData.map( (row, index) => (
+			            <TableRow key={index} selected={row.selected}>
+			              <TableRowColumn>{row.ranking}</TableRowColumn>
+			              <TableRowColumn>{row.name}</TableRowColumn>
+			              <TableRowColumn>{row.usage}</TableRowColumn>
+			            </TableRow>
+			            ))}
+			        </TableBody>
+			      </Table>
+			    </div>
 			<br/>
 			</Card>
 		)
