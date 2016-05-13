68616b3f5239e67cdebed7526a53574c3c5dd88e
Inventrax <inventrax@gmail.com>
2016-05-13 16:58:15 +0200

    added selection for time periods on ranking + bugfixes

--- client/src/app/PageComponents/Friends.jsx
+++ client/src/app/PageComponents/Friends.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Card from 'material-ui/Card/Card';
 import CardHeader from 'material-ui/Card/CardHeader';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 import Table from 'material-ui/Table/Table';
 import TableHeaderColumn from 'material-ui/Table/TableHeaderColumn';
 import TableRow from 'material-ui/Table/TableRow';
@@ -17,8 +19,10 @@ const styles = {
 }
 
 
-
 const Friends = React.createClass({
+
+
+
 	getInitialState: function () {
 		return {
 			loading: true,
@@ -27,8 +31,8 @@ const Friends = React.createClass({
 		};
 	},
 	handleChange: function (event, index, value) {
-		this.setState({value});
-	},
+		this.setState({value});	},
+
 	componentDidMount: function () {
 		if (!this.props.rest) throw "Notification Error: No rest client provided!";
 
@@ -55,102 +59,81 @@ const Friends = React.createClass({
 			});
 		}.bind(this));
 	},
+
+
+
 	render: function() {
+		
+
+		const period = [
+			<MenuItem key={1} value={1} primaryText="Weekly" />,
+			<MenuItem key={2} value={2} primaryText="Monthly" />,
+			<MenuItem key={3} value={3} primaryText="Yearly" />,
+		];
+		let timePeriod;
 		
 		let FriendList= [];
 		let friend = {};
 
-		if (!this.state.loading) {
-			for (let i = 0; i < this.state.friends.length; i++) {
-				FriendList.push(friend = {ranking: i+1, name: this.state.friends[i].attributes.first_name+" "+this.state.friends[i].attributes.last_name, usage: Math.floor(Math.random()*20), households: this.state.friends[i].relationships.owned_homes.meta.count});
+
+		function makeFriendList (TimePeriod, test) {
+			for (let i = 0; i < test.state.friends.length; i++) {
+				FriendList.push(friend = {
+					ranking: i+1, 
+					name: test.state.friends[i].attributes.first_name+" "+test.state.friends[i].attributes.last_name, 
+					usage: Math.floor(Math.random()*20), 
+					households: test.state.friends[i].relationships.owned_homes.meta.count,
+				});
 			}
+			FriendList.sort(function(a, b){
+			    let keyA = new Date(a.usage/a.households),
+					keyB = new Date(b.usage/b.households);
+				if(keyA < keyB) return -1;
+				if(keyA > keyB) return 1;
+				return 0;
+			});
+			return FriendList;
 		}
-
-		FriendList.sort(function(a, b){
-		    let keyA = new Date(a.usage),
-				keyB = new Date(b.usage);
-				// Compare the 2 dates
-			if(keyA < keyB) return -1;
-			if(keyA > keyB) return 1;
-			return 0;
-		});
-
-
-		const tableData = FriendList;/*[
-			{
-				ranking: '1',
-				name: 'John Smith',
-				usage: 'Employed',
-				households: '3',
-			},
-			{
-				ranking: '2',
-				name: 'Randal White',
-				usage: 'Unemployed',
-				households: '4',
-			},
-			{
-				ranking: '3',
-				name: 'Stephanie Sanders',
-				usage: 'Employed',
-				households: '1',
-			},
-			{
-				ranking: '4',
-				name: 'Steve Brown',
-				usage: 'Employed',
-				households: '5',
-			},
-			{
-				ranking: '5',
-				name: 'Joyce Whitten',
-				usage: 'Employed',
-				households: '1',
-			},
-			{
-				ranking: '6',
-				name: 'Samuel Roberts',
-				usage: 'Employed',
-				households: '2',
-			},
-			{
-				ranking: '7',
-				name: 'Adam Moore',
-				usage: 'Employed',
-				households: '2',
-			},
-		];*/
 		
 		return (
 			<Card>
 			<CardHeader
 				title="Friends"
 				subtitle={this.state.myName}/>
+
 				<div>
-			      <Table
-			        height={this.state.height}>
-			        <TableHeader
-			        	displaySelectAll={false}>
-			          <TableRow>
-			            <TableHeaderColumn tooltip="Rank">Ranking</TableHeaderColumn>
-			            <TableHeaderColumn tooltip="Name">Name</TableHeaderColumn>
-			            <TableHeaderColumn tooltip="Total Usage">Usage</TableHeaderColumn>
-			            <TableHeaderColumn tooltip="Amount of Households">Households</TableHeaderColumn>
-			          </TableRow>
-			        </TableHeader>
-			        <TableBody>
-			          {tableData.map( (row, index) => (
-			            <TableRow key={index} selected={row.selected}>
-			              <TableRowColumn>{index+1}</TableRowColumn>
-			              <TableRowColumn>{row.name}</TableRowColumn>
-			              <TableRowColumn>{row.usage}</TableRowColumn>
-			              <TableRowColumn>{row.households}</TableRowColumn>
-			            </TableRow>
-			            ))}
-			        </TableBody>
-			      </Table>
-			    </div>
-			<br/>
+					<SelectField
+						value={this.state.value}
+						onChange={this.handleChange}
+						autoWidth={true}
+						floatingLabelText="Time period"
+						timePeriod = {this.state.value}>
+						{period}
+					</SelectField>
+				<br />
+					<Table
+						height={this.state.height}>
+						<TableHeader
+							displaySelectAll={false}>
+							<TableRow>
+								<TableHeaderColumn tooltip="Rank">Ranking</TableHeaderColumn>
+								<TableHeaderColumn tooltip="Name">Name</TableHeaderColumn>
+								<TableHeaderColumn tooltip="Total Usage">Usage</TableHeaderColumn>
+								<TableHeaderColumn tooltip="Amount of Households">Households</TableHeaderColumn>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{makeFriendList(timePeriod, this).map( (row, index) => (
+								<TableRow key={index} selected={row.selected}>
+									<TableRowColumn>{index+1}</TableRowColumn>
+									<TableRowColumn>{row.name}</TableRowColumn>
+									<TableRowColumn>{row.usage}</TableRowColumn>
+									<TableRowColumn>{row.households}</TableRowColumn>
+								</TableRow>
+			 				))}
+						</TableBody>
+					</Table>
+				</div>
 			</Card>
 		)
 	},
