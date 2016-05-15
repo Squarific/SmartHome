3b36f9212a05d5d4049b59691d7c9ac006895d74
Filip Smets <alemaaltevinden@gmail.com>
2016-05-15 17:17:32 +0200

    Update sensorListButton

--- client/src/app/Components/SensorListButton.jsx
+++ client/src/app/Components/SensorListButton.jsx
@@ -4,7 +4,7 @@ import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
 
 const styles = {
-	sharegraphbutton: {
+	sensorlistbutton: {
 		display: "inline-block",
 		textAlign: "center",
 	},
@@ -20,92 +20,39 @@ const SensorListButton = React.createClass({
 	componentDidMount: function () {
 		if (!this.props.rest) throw "SensorListButton Error: No rest client provided!";
 
-		// this.props.rest.get(["api", "users", "me"], {}, function (data) {
-		// 	if (data.error) {
-		// 		console.log(data.error);
-		// 		return;
-		// 	}
-
-		// 	this.setState({
-		// 		myId: data.data.id,
-		// 		loading: false,
-		// 	});
-		// }.bind(this));
 	},
 	handleChange: function (event, index, value) {
 		this.setState({value});
 	},
-	handleShareGraphClose: function () {
+	handleSensorListClose: function () {
 		this.setState({
-			shareGraphOpen: false,
+			sensorListOpen: false,
 		});
 	},
-	handleShareGraphOpen: function () {
+	handleSensorListOpen: function () {
 		this.setState({
-			shareGraphOpen: true,
+			sensorListOpen: true,
 		});
 	},
-	handleTextFieldChange: function (e) {
-		this.setState({
-            postMessage: e.target.value,
-        });
-	},
-
-	postMessage: function () {
-		if ((this.state.postMessage !== "") && (!this.state.loading)) {
-			this.props.rest.post(["api", "posts"], {
-	            content: this.state.postMessage,
-	            plot: JSON.stringify(this.props.data),
-	            read: false,
-	            user: this.state.myId,
-			}, function (data) {
-				if (data.error) {
-					console.log(data.error);
-					return;
-				}
-			}.bind(this));
-
-			this.setState({
-				shareGraphOpen: false,
-				postMessage: "",
-			});
-		}
-	},
-
 	render: function() {
-		const shareGraphActions = [
+		const sensorListActions = [
 			<FlatButton style={styles.cancelButton}
 				label={this.props.lang.cancel}
 				secondary={true}
-				onTouchTap={this.handleShareGraphClose}/>,
-
-			<FlatButton style={styles.postButton}
-				label={this.props.lang.post}
-				primary={true}
-				keyboardFocused={true}
-				onTouchTap={this.postMessage}/>,
+				onTouchTap={this.handleSensorListClose}/>,
 		];
 
 		return (
-			<div style={styles.sharegraphbutton}>
-				<FlatButton label={this.props.lang.shareGraph}
-					onTouchTap={this.handleShareGraphOpen}
+			<div style={styles.sensorlistbutton}>
+				<FlatButton label={this.props.lang.sensors}
+					onTouchTap={this.handleSensorListOpen}
 					primary={true}/>
 
-				<Dialog style={styles.sharegraphbutton}
-					title={this.props.lang.shareGraph}
-					open={this.state.shareGraphOpen}
-					onRequestClose={this.handleShareGraphClose}
-					actions={shareGraphActions}>
-
-					<form className="shareGraph">
-						<TextField hintText="" floatingLabelText={this.props.lang.message}
-							multiLine={true}
-							rows={5}
-							rowsMax={5}
-							value={this.state.postMessage}
-							onChange={this.handleTextFieldChange}/>
-					</form>
+				<Dialog style={styles.sensorlistbutton}
+					title={this.props.lang.sensors}
+					open={this.state.sensorListOpen}
+					onRequestClose={this.handleSensorListClose}
+					actions={sensorListActions}>
 				</Dialog>
 			</div>
 		)
